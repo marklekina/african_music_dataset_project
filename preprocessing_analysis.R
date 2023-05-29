503b9236b3a49a2aa17518c37c376d57f4cb0e47
Mark Lekina Rorat <marklekina@gmail.com>
2023-05-28 22:09:22 -0400

    computed proportion of collaborations by country

--- preprocessing_analysis.R
+++ preprocessing_analysis.R
@@ -6,16 +6,18 @@ library(stringr)
 path_to_artists <- "data/artists/"
 path_to_tracks <- "data/tracks/"
 
-artist_spotify_data <- read_csv(paste0(path_to_artists, "artist_spotify_data.csv"))
-track_spotify_data <- read_csv(paste0(path_to_tracks, "track_spotify_data.csv"))
+artist_spotify_data <- read_csv(paste0(path_to_artists, "spotify_data.csv"))
+track_spotify_data <- read_csv(paste0(path_to_tracks, "spotify_data.csv"))
 nationality_data <- read_csv(paste0(path_to_artists, "nationalities.csv"))
 
 # explore the data
 View(artist_spotify_data)
 View(track_spotify_data)
+View(nationality_data)
 
-colnames(artist_spotify_data)
-colnames(track_spotify_data)
+glimpse(artist_spotify_data)
+glimpse(track_spotify_data)
+glimpse(nationality_data)
 
 ## preprocessing
 
@@ -39,38 +41,83 @@ track_data <- track_spotify_data %>%
 # note that some tracks have the same IDs but are essentially different releases of the same song, usually in different compilations.
 # at this stage, we do not expect duplicate IDs, since we already filtered them out in the data collection stage.
 
+# function to remove whitespace and convert track name to lowercase
+remove_whitespace_lowecase <- function(track_name) {
+    track_name <- track_name %>%
+        str_remove_all("\\s+") %>%
+        str_to_lower()
+
+    return(track_name)
+}
+
 # filter out these duplicate tracks
 track_data_dedup <- track_data %>%
     arrange(-popularity) %>%
     mutate(
-        track_name_processed = process_names(track_name),
+        track_name_processed = remove_whitespace_lowecase(track_name),
         is_duplicate = duplicated(track_name_processed, artist_ids_vector)
     ) %>%
     filter(!is_duplicate) %>%
     select(-track_name_processed, -is_duplicate)
 
-# subset to tracks with at least two artist IDs, i.e. collaborations
-collaborative_track_data <- track_data_dedup %>%
-    mutate(num_artists = lapply(artist_ids_vector, length)) %>%
-    filter(num_artists > 1)
+## Analysis
 
+# 1. Which country's artists have the highest proportion of collaborations?
 
-# function to return a vector of country names from a vector of artist IDs
+# mark collaborations and single-artist tracks
+track_data_clean <- track_data_dedup %>%
+    mutate(num_artists = sapply(artist_ids_vector, length))
+
+# function to return the country names of an artist with the given ID
 get_country_name <- function(artist_id, artist_nationality_data) {
     # get country name from artist ID
     match <- artist_nationality_data %>%
         filter(id %in% artist_id)
 
     if (nrow(match) > 0) {
-        return(pull(match, country))
+        country_name <- pull(match, country)
+        return(country_name)
     }
-    return("")
+    return(NA)
 }
 
-# add a column of artist country names to the collaborative track data
-track_data_with_countries <- collaborative_track_data %>%
-    mutate(artist_countries = sapply(artist_ids_vector, function(vector) {
-        map_chr(vector, ~ get_country_name(.x, artist_data)) %>%
-            .[nzchar(.)] %>%
-            na.omit()
-    }))
+# subset to tracks with at least one African artist
+track_data_clean <- track_data_clean %>%
+    mutate(artist_countries = map(artist_ids_vector, ~ get_country_name(.x, artist_data))) %>%
+    filter(!is.na(artist_countries))
+
+# compute country appearance frequencies for collaborative tracks
+collaborative_country_counts <- track_data_clean %>%
+    filter(num_artists > 1) %>%
+    pull(artist_countries) %>%
+    unlist() %>%
+    table() %>%
+    as.data.frame() %>%
+    arrange(-Freq)
+colnames(collaborative_country_counts) <- c("country", "collaboration_count")
+
+# compute country appearance frequencies for single-artist tracks
+single_artist_country_counts <- track_data_clean %>%
+    filter(num_artists == 1) %>%
+    pull(artist_countries) %>%
+    unlist() %>%
+    table() %>%
+    as.data.frame() %>%
+    arrange(-Freq)
+colnames(single_artist_country_counts) <- c("country", "single_artist_count")
+
+# join the two dataframes to compute the proportion of collaborative tracks for each country
+country_counts_df <- collaborative_country_counts %>%
+    full_join(single_artist_country_counts, by = "country") %>%
+    mutate(
+        collaboration_count = replace(collaboration_count, is.na(collaboration_count), 0),
+        collaboration_proportion = collaboration_count / (collaboration_count + single_artist_count)
+    ) %>%
+    arrange(-collaboration_proportion)
+
+View(country_counts_df)
+
+# 2. Which genres tend to have a higher frequency of collaborations?
+# 3. Is there a correlation between collaboration and artistic success?
+# 4. How does collaboration impact overall artist success?
+# 5. How has collaboration between African artists evolved over time?
